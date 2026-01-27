734fec2b3fd55a4ad06f03517dc0acbc64c1ddc8
machao <machao@taou.com>
2026-01-27 19:54:17 +0800

    github action config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // 部署到 GitHub Pages 时，这里填写你的 GitHub Pages 网址
   // 例如：https://<username>.github.io
-  site: 'https://username.github.io',
+  site: 'https://0x4b-traveler.github.io/',
   
   // 如果你的仓库名称不是 <username>.github.io，则需要设置 base 为仓库名称
   // 例如：如果仓库名是 my-blog，则设置为 '/my-blog'
